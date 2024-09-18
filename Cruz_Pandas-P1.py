f7b9c004f19d217e84307ca1877e5ca6c007acf9
EnzoZurc <christianenzo.cruz.eng@ust.edu.ph>
2024-09-18 22:04:44 +0800

    Add files via upload

---
+++ Cruz_Pandas-P1.py
@@ -0,0 +1,67 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ec9d06e1-b9e0-450c-a53c-938312d2d03f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#To access Panda Library we need to import\n",
+    "import pandas as pd"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "442fe894-ede0-4338-942e-7ad57f49b167",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Loading the csv file\n",
+    "cars = pd.read_csv('cars.csv')\n",
+    "cars"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "abbe3eb8-6178-4980-8210-aabe541327cd",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Displaying the first 5 rows\n",
+    "cars.head(5)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3e19674c-06d7-4139-93bc-67d212dcce1b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
